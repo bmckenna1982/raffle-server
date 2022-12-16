37b18d4ea87af800f7a891ce64644f6b95baa86b
Brian McKenna <bmckenna1982@gmail.com>
2022-12-16 10:43:34 -0500

    updated log statement for testing

--- src/server.js
+++ src/server.js
@@ -16,5 +16,5 @@ const db = knex({
 app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT} with ${DATABASE_URL}`);
 })
